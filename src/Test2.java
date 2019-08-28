29a341926a39129369e303ed64f70ebac611f4f3
qmszlb <36808182+qmszlb@users.noreply.github.com>
2019-08-28 09:25:38 +0800

    多线程

--- src/Test2.java
+++ src/Test2.java
@@ -1,4 +1,27 @@
-public class Test2 {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+public class Test2 implements  Runnable {
+   @Override
+   public void run() {
+      for (int i = 0; i <20 ; i++) {
+         System.out.println("一边听歌"+i);
+      }
+   }
+
+   public static void main(String[] args) {
+        Test2 test2 = new Test2();
+//      Thread t = new Thread(test2);
+//      t.start();
+
+
+
+      new Thread (test2).start();
+      for (int i = 0; i <20 ; i++) {
+         System.out.println("一边condig"+i);
+
+      }
+   }
 
 
 }
